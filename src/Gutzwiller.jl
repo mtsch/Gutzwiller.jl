d50341d7df22f87c6ab8db8df08a1deed54b3131
mtsch <matijacufar@gmail.com>
2024-01-12 14:35:15 +1300

    cleaning

--- src/Gutzwiller.jl
+++ src/Gutzwiller.jl
@@ -17,10 +17,7 @@ export kinetic_vqmc, kinetic_vqmc!, local_energy_estimator, KineticVQMC
 
 include("utils.jl")
 include("ansatz.jl")
-export GutzwillerAnsatz
-
 include("localenergy.jl")
-export LocalEnergy
 
 include("kinetic-vqmc/state.jl")
 include("kinetic-vqmc/qmc.jl")
