559da0f7512e6961bf600a300f6bed7e7b7840a0
mtsch <matijacufar@gmail.com>
2024-01-15 13:11:49 +1300

    Cleanup, initial amsgrad implementation

--- src/Gutzwiller.jl
+++ src/Gutzwiller.jl
@@ -1,9 +1,15 @@
 module Gutzwiller
 
-using Rimu
-using VectorInterface
+using DataFrames
 using Folds
+using Measurements
 using ProgressMeter
+using Parameters
+using Rimu
+using Statistics
+using StatsBase
+using StaticArrays
+using VectorInterface
 
 include("deterministic-evaluator.jl")
 export GutzwillerEvaluator
@@ -17,5 +23,7 @@ export GutzwillerQMCEvaluator
 
 include("optimization.jl")
 export gutz_optimize
+
+include("amsgrad.jl")
 
 end
---
+++ src/amsgrad.jl
@@ -0,0 +1,53 @@
+# m[i] = f(grad)
+# n[i] = g(grad)
+# delta_p[j] = -αm[j]/√n[j] # update to params
+
+# TODO: _continue_ computations to prevent issues with warmup.
+#
+
+function amsgrad(f, params; kwargs...)
+    params_svec = SVector{length(params)}(params)
+    return amsgrad(f, params_svec; kwargs...)
+end
+function amsgrad(
+    f, params::SVector{N,T};
+    maxiter=100, α=0.01, β1=0.1, β2=β1^2, verbose=true,
+    gtol=√eps(Float64),
+    ptol=√eps(Float64),
+) where {N,T}
+    df = DataFrame()
+
+    first_moment = zeros(SVector{N,T})
+    second_moment = zeros(SVector{N,T})
+
+
+    val, grad = val_and_grad(f, params)
+    first_moment = grad
+
+    for iter in 1:maxiter
+        val, grad = val_and_grad(f, params)
+        first_moment = (1 - β1) * first_moment - β1 * grad
+        second_moment = max.((1 - β2) * second_moment + β2 * grad.^2, second_moment)
+        Δparams = -α .* first_moment ./ sqrt.(second_moment)
+
+        if verbose
+            print(stderr, "Step $iter:")
+            print(stderr, " y: ", round(val, sigdigits=5))
+            print(stderr, ", x: ", round.(params, sigdigits=5))
+            print(stderr, ", Δx: ", round.(Δparams, sigdigits=5))
+            println(stderr, ", ∇: ", round.(grad, sigdigits=5))
+        end
+
+
+        push!(df, (; iter, val, grad, params, Δparams))
+        params += Δparams
+
+        if norm(Δparams) < ptol
+            break
+        end
+        if norm(grad) < gtol
+            break
+        end
+    end
+    return df
+end
