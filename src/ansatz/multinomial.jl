fc5b43d067f3ee6418e359749741a8e5aa98cefe
mtsch <matijacufar@gmail.com>
2024-02-12 17:04:49 +1300

    Make multinomial work with CompositeFS

--- src/ansatz/multinomial.jl
+++ src/ansatz/multinomial.jl
@@ -9,22 +9,38 @@ struct MultinomialAnsatz{K} <: Gutzwiller.AbstractAnsatz{K,Float64,1}
     normalization::Float64
 end
 function MultinomialAnsatz(H::AbstractHamiltonian; normalize=false)
-    K = typeof(starting_address(H))
+    addr = starting_address(H)
+    K = typeof(addr)
     N = float(num_particles(K))
     if normalize
-        normalization = gamma(num_particles(K) + 1) / float(num_modes(K))^num_particles(K)
+        normalization = multinomial_normalization(addr)
     else
         normalization = 1.0
     end
     return MultinomialAnsatz{K}(normalization)
 end
 
-function multinomial_weight(addr, occ=OccupiedModeMap(addr))
-    return prod(idx -> 1/gamma(idx.occnum + 1), occ)
+function multinomial_normalization(addr::BoseFS)
+    return gamma(num_particles(addr) + 1) / float(num_modes(addr))^num_particles(addr)
+end
+function multinomial_normalization(addr::FermiFS)
+    return 1.0
+end
+function multinomial_normalization(addr::CompositeFS)
+    return prod(multinomial_normalization, addr.components)
 end
 
-(b::MultinomialAnsatz)(addr, p) = (multinomial_weight(addr) * b.normalization)^p[1]
+function multinomial_weight(addr::BoseFS, occ=OccupiedModeMap(addr))
+    return prod(idx -> 1/gamma(idx.occnum + 1), occ)
+end
+multinomial_weight(addr::FermiFS) = 1.0
+multinomial_weight(addr::CompositeFS) = multinomial_weight(addr.components)
+multinomial_weight(addrs::Tuple{}) = 1.0
+multinomial_weight((a, as...)::Tuple) = multinomial_weight(a) * multinomial_weight(as)
 
+function (b::MultinomialAnsatz)(addr, p)
+    return (multinomial_weight(addr) * b.normalization)^p[1]
+end
 function val_and_grad(b::MultinomialAnsatz, addr, param)
     p = only(param)
     y = multinomial_weight(addr) * b.normalization
