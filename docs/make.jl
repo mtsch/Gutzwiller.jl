b576974259dd4d74b7fa7e8c97a6ef14fbc099d5
mtsch <matijacufar@gmail.com>
2024-09-16 14:50:15 +1200

    add Optim to test deps

--- docs/make.jl
+++ docs/make.jl
@@ -4,4 +4,4 @@ Literate.markdown(
     joinpath(@__DIR__, "README.jl");
     flavor=Literate.CommonMarkFlavor(), execute=true,
 )
-mv(joinpath(@__DIR__, "README.md"), joinpath(@__DIR__, "../README.md"))
+mv(joinpath(@__DIR__, "README.md"), joinpath(@__DIR__, "../README.md"); force=true)
